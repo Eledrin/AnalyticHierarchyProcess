c0ebc59001eef21a9a8eacc4338197746294fc30
Damian Spaczyński <d.spaczynski@outlook.com>
2016-11-06 17:12:46 +0100

    project organization; add classes: AHP and Criterion; first conception.

---
+++ AnalyticHierarchyProcess/AnalyticHierarchyProcess/Classes/AHP.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalyticHierarchyProcess.Classes
+{
+    public class AHP
+    {
+        private static AHP anchorInstance = null;
+        public static AHP Instance
+        {
+            get
+            {
+                if (anchorInstance == null)
+                {
+                    anchorInstance = new AHP();
+                }
+                return anchorInstance;
+            }
+        }
+        public int NumberOfAlternatives { get; }
+        public List<string> AlternativesNames { get; }
+        public List<int> WeightCriteria { get; }
+
+        private AHP() { }
+
+    }
+}
---
+++ AnalyticHierarchyProcess/AnalyticHierarchyProcess/Classes/Criterion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalyticHierarchyProcess.Classes
+{
+    public class Criterion
+    {
+        public string Name { get; }
+        public Criterion(string name)
+        {
+            Name = name;
+        }
+    }
+}
